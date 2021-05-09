84510cf1db4316ede074c140d4f6139c1edecaa4
Narmdo <narmdo@gmail.com>
2021-05-10 00:07:27 +1000

    Updated how tokens are accessed

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # att-websockets
 Websocket connections for ATT
+
+
+
+# Create a connection
+`let serverId = 123;
+let connectionName = "Test";
+
+let connection = new Connection(serverId, connectionName);
+
+connection.onMessage = console.log;
+
+try
+{
+    await connection.open();
+
+    connection.send("player kill Joel");
+}
+catch (e)
+{
+    console.error("Error connecting to server");
+    console.error(e);
+}`
+
+# Use the basic wrapper
+This is recommended for 90% of cases, as it handles subscriptions and command responses
+
+`let wrapper = new BasicWrapper(connection);
+
+var result = await wrapper.send("spawn joel dynamite");
+
+wrapper.subscribe("PlayerJoined", console.log);`
--- src/connection.ts
+++ src/connection.ts
@@ -2,119 +2,228 @@ import { Sessions, Servers } from 'alta-jsapi';
 
 import WebSocket from 'isomorphic-ws';
 
+import jwt from 'jsonwebtoken';
+
+import nodeFetch from 'node-fetch';
+
+try
+{
+    var fetch = nodeFetch.bind(window);
+}
+catch
+{
+    var fetch = nodeFetch;
+}
+
 export enum MessageType
 {
-  SystemMessage = "SystemMessage",
-  Subscription = "Subscription",
-  CommandResult = "CommandResult"
+    SystemMessage = "SystemMessage",
+    Subscription = "Subscription",
+    CommandResult = "CommandResult"
 }
 
 export type Message =
 {
-  id: number,
-  type: MessageType,
-  timeStamp: string,
-  eventType?: string,
-  data: any,
-  commandId?: number
+    id: number,
+    type: MessageType,
+    timeStamp: string,
+    eventType?: string,
+    data: any,
+    commandId?: number
+}
+
+class ConnectionError extends Error
+{
+    details:any;
+
+    constructor(message:string, details:any)
+    {
+        super(message);
+
+        this.details = details;
+    }
+}
+
+export interface AccessProvider
+{
+    serverId: number;
+    token: string;
+    decoded:any;
+    ipAddress: string;
+    webserverPort: number;
+    websocketPort: number;
+
+    check() : Promise<void>;
+}
+
+export class JsapiAccessProvider implements AccessProvider
+{
+    serverId: number;
+
+    token: string = '';
+    decoded: any = null;
+    ipAddress: string = '127.0.0.1';
+    webserverPort: number = 1760;
+    websocketPort: number = 1761;
+
+    constructor(serverId:number)
+    {
+        this.serverId = serverId;
+    }
+    
+    async check()
+    {
+        if (!this.decoded || this.decoded.exp - new Date().getTime() / 1000 < 15)
+        {
+            var details = await Servers.joinConsole(this.serverId, false);
+
+            if (details.allowed)
+            {
+                this.ipAddress = details.connection.address || this.ipAddress;
+                this.websocketPort = details.connection.websocket_port || this.websocketPort;
+                this.webserverPort = details.connection.webserver_port || this.webserverPort;
+                this.token = details.token;
+                this.decoded = jwt.decode(this.token);
+            }
+            else
+            {
+                throw new ConnectionError("Connection rejected", details);
+            }
+        }
+    }
 }
 
 export default class Connection
 {
-  name:string;
+    serverId: number;
+    name: string;
 
-  connection:WebSocket|undefined;
+    access: AccessProvider;
 
-  onMessage:(response:Message)=>void = console.log;
+    connection: WebSocket | undefined;
 
-  onError = console.error;
+    onMessage: (response: Message) => void = console.log;
 
-  onClose = console.error;
+    onError = console.error;
 
-  nextSendId = 0;
-  nextReceiveId = 0;
+    onClose = console.error;
 
-  constructor(name:string)
-  {
-    this.name = name;
-  }
+    nextSendId = 0;
+    nextReceiveId = 0;
 
-  async connect(ipAddress:string, port:string|number, token:string)
-  {
-    console.log(`Connecting to ${ipAddress}:${port}`);
-
-    const connection = new WebSocket(`ws://${ipAddress}:${port}`);
-    this.connection = connection;
-
-    await new Promise((resolve, reject) => 
+    constructor(id: AccessProvider|number, name: string)
     {
-      connection.onopen = () => 
-      {
-        connection.onmessage = (message:any) => 
+        if (id instanceof Object)
         {
-            var data = JSON.parse(message.data);
-
-            if (data.type == 'FatalLog' || data.type == 'ErrorLog')
-            {
-                console.error("Failing connection!");
-                console.error(message);
-                reject(data.data);
-            }
-            else
-            {
-                console.log("Resolving connection!");
-                console.log(data.data);
-                resolve();
-            }
+            this.serverId = id.serverId;
+            this.access = id;
+        }
+        else
+        {
+            this.serverId = id;
+            this.access = new JsapiAccessProvider(id);
         }
 
-        connection.send(token);
-      };
-      
-      connection.onerror = reject;
-    });
-
-    connection.onmessage = this.handleMessage.bind(this);
-    connection.onerror = (error:any) => this.onError(error);
-    connection.onclose = (data:any) => this.onClose(data);
-  }
-
-  handleMessage(message:any)
-  {
-    var data:Message = JSON.parse(message.data);
-
-    data.id = this.nextReceiveId++;
-
-    this.onMessage(data);
-  }
-
-  send(content:string) : number
-  {    
-    if (!this.connection)
-    {
-      console.error("Connection not started. Call 'connect' first!");
-      return -1;
+        this.name = name;
     }
 
-    var id = this.nextSendId++;
-
-    this.connection.send(JSON.stringify({id, content}));
-
-    return id;
-  }
-
-  terminate() 
-  {
-    try 
+    async open()
     {
-      if (!!this.connection)
-      {
-        this.connection.close();
-      }
-    }  
-    catch (error) 
-    {
-      console.log(`Error destroying connection : ${error}`);
+        await this.access.check();
+
+        var { ipAddress, websocketPort, token } = this.access;
+
+        console.log(`Connecting to ${ipAddress}:${websocketPort}`);
+
+        const connection = new WebSocket(`ws://${ipAddress}:${websocketPort}`);
+        this.connection = connection;
+
+        await new Promise((resolve, reject) => 
+        {
+            connection.onopen = () => 
+            {
+                connection.onmessage = (message: any) => 
+                {
+                    var data = JSON.parse(message.data);
+
+                    if (data.type == 'FatalLog' || data.type == 'ErrorLog')
+                    {
+                        console.error("Failing connection!");
+                        console.error(message);
+                        reject(data.data);
+                    }
+                    else
+                    {
+                        console.log("Resolving connection!");
+                        console.log(data.data);
+                        resolve();
+                    }
+                }
+
+                connection.send(token);
+            };
+
+            connection.onerror = reject;
+        });
+
+        connection.onmessage = this.handleMessage.bind(this);
+        connection.onerror = (error: any) => this.onError(error);
+        connection.onclose = (data: any) => this.onClose(data);
     }
-  }
+
+
+    async download(downloadUrl:string)
+    {
+        await this.access.check();
+
+        var { ipAddress, webserverPort, token, decoded } = this.access;
+
+        return await fetch(`${ipAddress}:${webserverPort}/output/${decoded.UserId}/${downloadUrl}`, { headers: { "Authorization": `Bearer ${token}` } });
+    }
+
+    handleMessage(message: any)
+    {
+        var data: Message = JSON.parse(message.data);
+
+        data.id = this.nextReceiveId++;
+
+        if (data.type == MessageType.CommandResult && !!data.data.Result.downloadUrl)
+        {
+            let file = data.data.Result.downloadUrl;
+
+            data.data.download = () => this.download(file);
+        }
+
+        this.onMessage(data);
+    }
+
+    send(content: string): number
+    {
+        if (!this.connection)
+        {
+            console.error("Connection not started. Call 'connect' first!");
+            return -1;
+        }
+
+        var id = this.nextSendId++;
+
+        this.connection.send(JSON.stringify({ id, content }));
+
+        return id;
+    }
+
+    terminate() 
+    {
+        try 
+        {
+            if (!!this.connection)
+            {
+                this.connection.close();
+            }
+        }
+        catch (error) 
+        {
+            console.log(`Error destroying connection : ${error}`);
+        }
+    }
 }
